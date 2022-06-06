51e1ba1eb72dfcb4a4c7058b914e95b814631e97
hypnos-coder <holycoder2002@gmail.com>
2022-06-06 12:00:38 -0500

    login implemented

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import javax.swing.*;
+import java.sql.*;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.print("Here is the beginning of our journey");
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        System.out.println("Here is the beginning of our journey");
+        LoginForm loginForm = new LoginForm(null);
+        Authentification authentification = new Authentification();
+        boolean test = authentification.login("amen","merbelle");
+        System.out.print(test);
+
     }
 }
