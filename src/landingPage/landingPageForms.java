b6156ade565d599222c8a42fb146b3cfdfbcb15b
reduwan <hassenreduwan@gmail.com>
2022-06-08 09:41:01 -0500

    Landing Page is almost there.

---
+++ src/landingPage/landingPageForms.java
@@ -0,0 +1,9 @@
+package landingPage;
+
+import javax.swing.*;
+
+public class landingPageForms {
+    private JTextField textField1;
+    private JButton contactUsButton;
+    private JComboBox comboBox1;
+}
