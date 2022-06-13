f9ceca359efa777f5a9fbfc786852ac6e4a6d092
amen <79221904+hypnos-coder@users.noreply.github.com>
2022-06-13 11:13:26 -0500

    Revert "hypnos-haffy"

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,5 @@
-import javax.swing.*;
-import java.sql.*;
-
 public class Main {
-    public static void main(String[] args) throws SQLException, ClassNotFoundException {
-        System.out.println("Here is the beginning of our journey");
-        //LoginForm loginForm = new LoginForm(null);
-        Authentification authentification = new Authentification();
-        //boolean test = authentification.login("amen","merbelle");
-        //String test = authentification.register("hafsa9","hafsabadullahi1@gmail.com","password123","password123", "6123091958");
-        //System.out.print(test);
-        RegistrationForm registrationForm = new RegistrationForm(null);
-        registrationForm.setVisible(true);
-
+    public static void main(String[] args) {
+        System.out.print("Here is the beginning of our journey");
     }
 }
