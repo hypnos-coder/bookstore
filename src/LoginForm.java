0af0dc2c491bfec33895a54dd3273b607318f05a
godfrey agaba <Godfrey7agaba@gmail.com>
2022-06-14 09:50:18 -0500

    final version login form( almost complete)

--- src/LoginForm.java
+++ src/LoginForm.java
@@ -17,73 +17,8 @@ public class LoginForm extends JDialog {
         setLocationRelativeTo(parent);
         setDefaultCloseOperation(DISPOSE_ON_CLOSE);
         setVisible(true);
-    
-    
-
-    /*btnOk.addActionListener(new ActionListener() {
-        @Override
-        public void actionPerformed(ActionEvent e) {
-            String email = tfEmail.getText();
-            String password = String.valueOf(pfPassword.getPassword());
-
-            user = getAuthenticatedUser(email, password);
-            
-            public user user;
-    private user getAuthentication(String email, String password) {
-        user user = null;
-        return user;
-
-
-            if (user != null) {
-                dispose();
-            } else {
-                JOptionPane.showMessageDialog(LoginForm.this,
-                        "Email or Password Invalid",
-                        "Try again",
-                        JOptionPane.ERROR_MESSAGE);
-            }
-        } */
 }
-
     public static void main(String[] args) {
         LoginForm loginForm = new LoginForm(null);
     }
-
-
 }
-/*public User user;
-private User getAuthenticatedUser(String email, String password){
-    User user = null;
-
-    String jdbc;
-    final String DB_URL = jdbc:mysql://localhost:3306""
-    final String USERNAME = "root";
-    final String PASSWORD = "Agagod12!@";
-
-    try{
-        Connection conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-        //Connected successfully
-
-        Statement stmt = conn.createStatement();
-        String sql = "SELECT*FROM users WHERE email=? AND password=?";
-        preparedStatement preparedStament = conn.prepareStatement(sql);
-        preparedStatement.setString(1, email);
-        preparedStatement.setString(2, password);
-
-        ResultSet resultSet = preparedStatement.executeQuery();
-
-
-
-
-    }catch(Exception e){
-        e.printStackTrace();
-    }
-
-
-    return user;
-}
-
-    public static void main(String[] args) {
-        LoginForm loginForm = new LoginForm(null);
-    }
-} */
